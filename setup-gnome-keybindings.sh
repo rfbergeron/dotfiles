6e15b9f8281b6e20ef9f7217ad0841f5879ad44d
rfbergeron <robertbergeron3@gmail.com>
2025-11-20 20:42:31 -0800

    Add/Update GNOME Keybind setup script

--- setup-gnome-keybindings.sh
+++ setup-gnome-keybindings.sh
@@ -13,11 +13,14 @@ gsettings set org.gnome.shell.keybindings toggle-message-tray "['<Super>m']"
 # custom keybinding to launch a terminal
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Terminal'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'xdg-terminal-exec'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>Return'
 
+if gsettings get org.gnome.shell.extensions.caffeine toggle-shortcut >/dev/null 2>&1; then
+	gsettings set org.gnome.shell.extensions.caffeine toggle-shortcut "['<Super>z']"
+fi
+
 # unset legacy and IBM Common User Access keybindings
-gsettings set org.gnome.desktop.wm.keybindings activate-window-menu "[]"
 gsettings set org.gnome.desktop.wm.keybindings begin-move "[]"
 gsettings set org.gnome.desktop.wm.keybindings begin-resize "[]"
 gsettings set org.gnome.desktop.wm.keybindings cycle-group "[]"
@@ -38,6 +41,7 @@ gsettings set org.gnome.shell.keybindings shift-overview-down "[]"
 gsettings set org.gnome.shell.keybindings shift-overview-up "[]"
 
 # change useful legacy and CUA keybindings to use the Super key
+gsettings set org.gnome.desktop.wm.keybindings activate-window-menu "['<Super>Menu']"
 gsettings set org.gnome.desktop.wm.keybindings close "['<Super>w']"
 gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "['<Super>r']"
 gsettings set org.gnome.desktop.wm.keybindings switch-applications \
@@ -51,13 +55,25 @@ gsettings set org.gnome.desktop.wm.keybindings switch-group-backward \
 
 gsettings set org.gnome.shell.keybindings show-screen-recording-ui \
 	"['<Super><Shift>r']"
+gsettings set org.gnome.shell.keybindings show-screenshot-ui \
+	"['<Super><Shift>s']"
+gsettings set org.gnome.shell.keybindings screenshot \
+	"[]"
+gsettings set org.gnome.shell.keybindings screenshot-window \
+	"[]"
 
 gsettings set org.gnome.settings-daemon.plugins.media-keys logout \
 	"['<Super><Shift>l']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys home \
+	"['<Super><Shift>h']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys eject \
+	"['<Super>e']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys www \
+	"['<Super><Shift>w']"
 
 # use numpad for window and workspace controls
 gsettings set org.gnome.desktop.wm.keybindings maximize \
-	"['<Super>Up','<Super>KP_Up']"
+	"[]"
 gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-down \
 	"['<Super><Shift>Down','<Super><Shift>KP_Down']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-left \
@@ -82,13 +98,14 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left \
 	"['<Super>Page_Up','<Super>KP_Page_Up']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right \
 	"['<Super>Page_Down','<Super>KP_Page_Down']"
-gsettings set org.gnome.desktop.wm.keybindings unmaximize \
-	"['<Super>Down','<Super>KP_Down']"
-
+gsettings set org.gnome.desktop.wm.keybindings toggle-maximized \
+	"['<Super>Up','<Super>KP_Up']"
 gsettings set org.gnome.mutter.keybindings toggle-tiled-left \
 	"['<Super>Left','<Super>KP_Left']"
 gsettings set org.gnome.mutter.keybindings toggle-tiled-right \
 	"['<Super>Right','<Super>KP_Right']"
+gsettings set org.gnome.desktop.wm.keybindings unmaximize \
+	"['<Super>Down','<Super>KP_Down']"
 
 # miscellaneous
 gsettings set org.gnome.settings-daemon.plugins.media-keys mic-mute \
@@ -103,4 +120,3 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys volume-mute \
 
 gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Super>d']"
 gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Super>f']"
-gsettings set org.gnome.desktop.wm.keybindings minimize "['<Super>h','<Super>z']"
